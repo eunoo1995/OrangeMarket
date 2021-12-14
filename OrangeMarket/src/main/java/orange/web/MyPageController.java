109207b222202df292a5beafb6285f74d3ca265e
eunoo1005@gmail.com <eunoo1995@gmail.com>
2021-12-14 20:08:27 +0900

    MypageController 내용 수정

--- OrangeMarket/src/main/java/orange/web/MyPageController.java
+++ OrangeMarket/src/main/java/orange/web/MyPageController.java
@@ -6,10 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MyPageController {
 
-	@RequestMapping(value="/history")
+	@RequestMapping(value="/mypage")
+	public String mypageMain() throws Exception {
+		
+		return "mypage/myPage";
+	}
+	
+	@RequestMapping(value="/trade-history")
 	public String history() throws Exception {
 		
-		return "mypage/history";
+		return "mypage/tradeHistory";
 	}
 	
 }
