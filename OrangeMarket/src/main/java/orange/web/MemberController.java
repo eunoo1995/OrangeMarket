b91c00afbca9f9476f856d3ef55bf2450bdf0314
habitdev <goodstory16@gmail.com>
2021-12-14 14:28:42 +0900

    Add: MemerController

---
+++ OrangeMarket/src/main/java/orange/web/MemberController.java
@@ -0,0 +1,21 @@
+package orange.web;
+
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import orange.service.DeptVO;
+import orange.service.OrangeService;
+
+@Controller
+public class MemberController {
+
+	@RequestMapping(value = "login")
+	public String deptlist(){
+				
+		return "member/login";
+	}	
+}
