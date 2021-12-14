ae78bbe011a09c71576d6a2e58b10ddf30f8ed8e
habitdev <goodstory16@gmail.com>
2021-12-14 14:52:45 +0900

    memberController import 제거

--- OrangeMarket/src/main/java/orange/web/MemberController.java
+++ OrangeMarket/src/main/java/orange/web/MemberController.java
@@ -1,14 +1,8 @@
 package orange.web;
 
-
-import javax.annotation.Resource;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import orange.service.DeptVO;
-import orange.service.OrangeService;
 
 @Controller
 public class MemberController {
