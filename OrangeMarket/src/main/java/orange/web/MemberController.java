d9690724f21084006b98cb96c692645db1d26eb9
habitdev <goodstory16@gmail.com>
2021-12-15 14:50:02 +0900

    수정된 pom.xml, pom.properties memberController 수정 건

--- OrangeMarket/src/main/java/orange/web/MemberController.java
+++ OrangeMarket/src/main/java/orange/web/MemberController.java
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MemberController {
 
-	@RequestMapping(value = "/login")
+	@RequestMapping(value = "login")
 	public String login(){
 				
 		return "member/login";
-	}	
+	}
+	
+	@RequestMapping(value = "join")
+	public String join(){
+				
+		return "member/joinTerms";
+	}
 }
