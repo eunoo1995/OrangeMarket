7b94070bbb2732c45fb27f694ef31f6b7bd69d94
habitdev <goodstory16@gmail.com>
2021-12-17 19:14:12 +0900

    memberController 수정

--- OrangeMarket/src/main/java/orange/web/MemberController.java
+++ OrangeMarket/src/main/java/orange/web/MemberController.java
@@ -1,10 +1,13 @@
 package orange.web;
 
+import java.util.Map;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import orange.service.MemberService;
@@ -38,14 +41,14 @@ public class MemberController {
 		return "member/joinForm";
 	}
 	
-	@RequestMapping(value="check-username")
+	@RequestMapping(value="check-usertel")
 	@ResponseBody
 	public String checkJoinTel(String tel) throws Exception {
 		
 		String msg = "";
 		int result = memberService.isMemberTel(tel);
 		
-		if(result != 0) {
+		if(result > 0) {
 			msg = "exit";
 		}
 		
@@ -53,30 +56,53 @@ public class MemberController {
 			msg = "ok";
 		}
 		
-		System.out.println("tel: " + msg);
-		
-		
+		//System.out.println("tel: " + msg);
 		return msg;
 	}
 	
 	@RequestMapping(value="check-nikname")
 	@ResponseBody
-	public String checkJoinNikname(String nickname) throws Exception {
-		
+	public String checkJoinNikname(@RequestParam Map<String, Object> param) throws Exception {
+
+		String nickname = (String) param.get("nickname");
 		String msg = "";
 		int result = memberService.isMemberNikname(nickname);
 		
-		if(result != 0) {
+		if(result > 0) {
 			msg = "exit";
 		}
 		
 		if(result == 0) {
 			msg = "ok";
 		}
-		
-		System.out.println(msg);
-		
+		/* System.out.println("nikname: " + msg); */
 		
 		return msg;
+	}
+	
+	@RequestMapping(value="check-password")
+	@ResponseBody
+	public String checkPassWord(@RequestParam Map<String, Object> param) throws Exception {
+
+		String password = (String) param.get("password");
+		String regPassword = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,20}$";
+		String msg = "";
+		
+		System.out.println("password : " + password);
+		
+		
+		if(password.matches(regPassword) == false) {
+			msg ="err";
+		} else {
+			msg = "ok";
+		}
+		
+		return msg;
+	}
+	
+	@RequestMapping("send-verif-email")
+	public String SendVerifEmail() {
+		
+		
 	}
 }
